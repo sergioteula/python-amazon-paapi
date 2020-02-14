32998df52ba722fcfa6021e88fe190f05894d28c
Sergio Abad <agutrot93@gmail.com>
2020-02-14 14:31:43 +0100

    Removed paapi5 SDK and added as requirement

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="python-amazon-paapi5",
-    version="0.1.1",
-    author="Sergio Abad",
-    author_email="sergio.abad@bytelix.com",
-    description="Amazon Product Advertising API V5 wrapper for Python",
+    name='python-amazon-paapi5',
+    version='1.0.0',
+    author='Sergio Abad',
+    author_email='sergio.abad@bytelix.com',
+    description='Amazon Product Advertising API 5.0 wrapper for Python',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/sergioteula/python-amazon-paapi5",
+    long_description_content_type='text/markdown',
+    license='GPL-3.0'
+    url='https://github.com/sergioteula/python-amazon-paapi5',
     packages=setuptools.find_packages(),
+    install_requires=['amightygirl.paapi5-python-sdk'],
     classifiers=[
-        "Programming Language :: Python",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=2.7',
 )
