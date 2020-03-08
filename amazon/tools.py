5852df3fef1e17f4c80b1c513eb23f5e317e5a67
Sergio Abad <agutrot93@gmail.com>
2020-03-08 13:21:36 +0100

    Updated tools and exception handling on paapi

--- amazon/tools.py
+++ amazon/tools.py
@@ -3,20 +3,14 @@
 import re
 
 
-def _chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
-
-
 def get_asin(url: str):
     """Find the ASIN from a given URL.
 
     Args:
-        url (string): The URL containing the product ASIN.
+        url (str): The URL containing the product ASIN.
 
     Returns:
-        string: Product ASIN. None if ASIN not found.
+        str: Product ASIN. None if ASIN not found.
     """
     # Return if url parameter already is the ASIN
     if re.search(r'^[A-Z0-9]{10}$', url):
@@ -24,3 +18,9 @@ def get_asin(url: str):
     # Extract ASIN from URL searching for alphanumeric and 10 digits
     have_asin = re.search(r'(dp|gp/product)/([a-zA-Z0-9]{10})', url)
     return have_asin.group(2) if have_asin else None
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
