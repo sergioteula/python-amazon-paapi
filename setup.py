628338a2edac606e201e0b5c7979f76f3076bb25
Sergio Abad <agutrot93@gmail.com>
2021-03-25 16:08:21 +0100

    Updated version, changelog and readme format

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='python-amazon-paapi',
-    version='3.3.1',
+    version='3.3.2',
     author='Sergio Abad',
     author_email='sergio.abad@bytelix.com',
     description='Amazon Product Advertising API 5.0 wrapper for Python',
