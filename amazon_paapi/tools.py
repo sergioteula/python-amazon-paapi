8cdc2cc9ecff77e64c992d16b85f86292e3d6ec2
Sergio Abad <agutrot93@gmail.com>
2021-07-07 19:06:42 +0200

    Added tests for get_asin

--- amazon_paapi/tools.py
+++ amazon_paapi/tools.py
@@ -3,20 +3,13 @@
 import re
 
 
-def get_asin(url: str):
-    """Find the ASIN from a given URL.
-
-    Args:
-        url (str): The URL containing the product ASIN.
-
-    Returns:
-        str: Product ASIN. None if ASIN not found.
-    """
+def get_asin(text: str) -> str:
+    """Returns the ASIN from a given text. Raises AsinNotFoundException on fail."""
     # Return if url parameter already is the ASIN
-    if re.search(r'^[A-Z0-9]{10}$', url):
-        return url
+    if re.search(r'^[A-Z0-9]{10}$', text):
+        return text
     # Extract ASIN from URL searching for alphanumeric and 10 digits
-    have_asin = re.search(r'(dp|gp/product|gp/aw/d|dp/product)/([a-zA-Z0-9]{10})', url)
+    have_asin = re.search(r'(dp|gp/product|gp/aw/d|dp/product)/([a-zA-Z0-9]{10})', text)
     return have_asin.group(2) if have_asin else None
 
 
