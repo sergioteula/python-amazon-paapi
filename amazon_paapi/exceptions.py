6255e5bf790b9da7c9c8427497dd8c2dc415ff41
Sergio Abad <agutrot93@gmail.com>
2021-07-07 20:25:00 +0200

    Refactored get_items method

--- amazon_paapi/exceptions.py
+++ amazon_paapi/exceptions.py
@@ -19,3 +19,15 @@ class InvalidArgumentException(AmazonException):
 class AsinNotFoundException(AmazonException):
     """Raised if the ASIN for an item is not found."""
     pass
+
+class ApiRequestException(AmazonException):
+    """Raised if the request to Amazon API fails"""
+    pass
+
+class MalformedRequestException(AmazonException):
+    """Raised if the request for Amazon API is not correctly formed"""
+    pass
+
+class ItemsNotFoudException(AmazonException):
+    """Raised if no items are found"""
+    pass
--- amazon_paapi/helpers/generators.py
+++ amazon_paapi/helpers/generators.py
@@ -1,4 +1,7 @@
-def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
+"""Module with helper functions for making generators."""
+
+
+def get_list_chunks(full_list, chunk_size):
+    """Yield successive chunks from list."""
+    for i in range(0, len(full_list), chunk_size):
+        yield full_list[i:i + chunk_size]
