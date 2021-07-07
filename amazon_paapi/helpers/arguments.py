558526352c7124fcd799a8631477cd4222206b46
Sergio Abad <agutrot93@gmail.com>
2021-07-07 19:41:09 +0200

    Added function get_items_ids

---
+++ amazon_paapi/helpers/arguments.py
@@ -0,0 +1,18 @@
+"""Module with helper functions for managing arguments."""
+
+
+from ..tools import get_asin
+from ..exceptions import InvalidArgumentException
+from typing import Union
+
+
+def get_items_ids(items: Union[str, list[str]]) -> list[str]:
+    if not isinstance(items, str) and not isinstance(items, list):
+        raise InvalidArgumentException('Invalid products argument, it should be a string or list of strings')
+
+    if isinstance(items, str):
+        items_ids = items.split(',')
+        return [get_asin(x.strip()) for x in items_ids]
+
+    else:
+        return [get_asin(x.strip()) for x in items]
