fc48bdd8c9850f87e4155f4c07c4dc1a5e39ae2b
Sergio Abad <agutrot93@gmail.com>
2021-07-07 19:29:28 +0200

    Rafactored get_asin function

--- amazon_paapi/tools.py
+++ amazon_paapi/tools.py
@@ -1,19 +1,18 @@
 """Some useful tools."""
 
+from amazon_paapi.exceptions import AsinNotFoundException
 import re
 
 
 def get_asin(text: str) -> str:
     """Returns the ASIN from a given text. Raises AsinNotFoundException on fail."""
-    # Return if url parameter already is the ASIN
+    # Return if text is an ASIN
     if re.search(r'^[A-Z0-9]{10}$', text):
         return text
+
     # Extract ASIN from URL searching for alphanumeric and 10 digits
-    have_asin = re.search(r'(dp|gp/product|gp/aw/d|dp/product)/([a-zA-Z0-9]{10})', text)
-    return have_asin.group(2) if have_asin else None
-
-
-def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
+    asin = re.search(r'(dp|gp/product|gp/aw/d|dp/product)/([a-zA-Z0-9]{10})', text)
+    if asin:
+        return asin.group(2)
+    else:
+        raise AsinNotFoundException('Asin not found found')
