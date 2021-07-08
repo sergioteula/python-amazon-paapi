80799e090920a50e6135468e6b111cd4c92ca483
Sergio Abad <agutrot93@gmail.com>
2021-07-08 20:40:30 +0200

    Added more type hints

--- amazon_paapi/helpers/generators.py
+++ amazon_paapi/helpers/generators.py
@@ -1,7 +1,10 @@
 """Module with helper functions for making generators."""
 
 
-def get_list_chunks(full_list, chunk_size):
+from typing import Generator
+
+
+def get_list_chunks(full_list: list[str], chunk_size: int) -> Generator[list[str], None, None]:
     """Yield successive chunks from list."""
     for i in range(0, len(full_list), chunk_size):
         yield full_list[i:i + chunk_size]
