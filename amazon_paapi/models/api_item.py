b1a4807d22e3e67a54a8bebff35165abea94ae2c
Sergio Abad <agutrot93@gmail.com>
2021-07-08 23:35:20 +0200

    Added type hints for all values

--- amazon_paapi/models/api_item.py
+++ amazon_paapi/models/api_item.py
@@ -1,21 +1,43 @@
 from .. import models
 
 """Shared models"""
-class ApiMultiValuedAttributeStr(models.MultiValuedAttribute):
+class ApiLabelLocale:
+    label: str
+    locale: str
+
+class ApiMultiValuedAttributeStr(ApiLabelLocale, models.MultiValuedAttribute):
     display_values: list[str]
 
 class ApiDisplayValuesType:
     display_value: str
     type: str
 
-class ApiMultiValuedAttributeType(models.MultiValuedAttribute):
+class ApiMultiValuedAttributeType(ApiLabelLocale, models.MultiValuedAttribute):
     display_values: list[ApiDisplayValuesType]
 
+class ApiUnitBasedAttribute(ApiLabelLocale, models.UnitBasedAttribute):
+    display_value: float
+    unit: str
+
+class ApiSingleStringValuedAttribute(ApiLabelLocale, models.SingleStringValuedAttribute):
+    display_value: str
+
+class ApiSingleBooleanValuedAttribute(ApiLabelLocale, models.SingleBooleanValuedAttribute):
+    display_value: bool
+
+class ApiSingleIntegerValuedAttribute(ApiLabelLocale, models.SingleIntegerValuedAttribute):
+    display_value: float
+
 """Image models"""
+class ApiImageSize(models.ImageSize):
+    url: str
+    height: str
+    width: str
+
 class ApiImageType(models.ImageType):
-    large: models.ImageSize
-    medium: models.ImageSize
-    small: models.ImageSize
+    large: ApiImageSize
+    medium: ApiImageSize
+    small: ApiImageSize
 
 class ApiImages(models.Images):
     primary: ApiImageType
@@ -23,20 +45,20 @@ class ApiImages(models.Images):
 
 """Item info models"""
 class ApiByLineInfo(models.ByLineInfo):
-    brand: models.SingleStringValuedAttribute
-    contributors: models.SingleStringValuedAttribute
-    manufacturer: models.SingleStringValuedAttribute
+    brand: ApiSingleStringValuedAttribute
+    contributors: ApiSingleStringValuedAttribute
+    manufacturer: ApiSingleStringValuedAttribute
 
 class ApiClassifications(models.Classifications):
-    binding: models.SingleStringValuedAttribute
-    product_group: models.SingleStringValuedAttribute
+    binding: ApiSingleStringValuedAttribute
+    product_group: ApiSingleStringValuedAttribute
 
 class ApiContentInfo(models.ContentInfo):
-    edition: models.SingleStringValuedAttribute
+    edition: ApiSingleStringValuedAttribute
     languages: ApiMultiValuedAttributeType
 
 class ApiContentRating(models.ContentRating):
-    audience_rating: models.SingleStringValuedAttribute
+    audience_rating: ApiSingleStringValuedAttribute
 
 class ApiExternalIds(models.ExternalIds):
     ea_ns: ApiMultiValuedAttributeStr
@@ -47,31 +69,36 @@ class ApiFeatures():
     features: ApiMultiValuedAttributeStr
 
 class ApiManufactureInfo(models.ManufactureInfo):
-    item_part_number: models.SingleStringValuedAttribute
-    model: models.SingleStringValuedAttribute
-    warranty: models.SingleStringValuedAttribute
+    item_part_number: ApiSingleStringValuedAttribute
+    model: ApiSingleStringValuedAttribute
+    warranty: ApiSingleStringValuedAttribute
 
 class ApiItemDimensions(models.DimensionBasedAttribute):
-    height: models.SingleIntegerValuedAttribute
-    length: models.SingleIntegerValuedAttribute
-    weight: models.SingleIntegerValuedAttribute
-    width: models.SingleIntegerValuedAttribute
+    height: ApiUnitBasedAttribute
+    length: ApiUnitBasedAttribute
+    weight: ApiUnitBasedAttribute
+    width: ApiUnitBasedAttribute
 
 class ApiProductInfo(models.ProductInfo):
-    color: models.SingleStringValuedAttribute
-    is_adult_product: models.SingleBooleanValuedAttribute
+    color: ApiSingleStringValuedAttribute
+    is_adult_product: ApiSingleBooleanValuedAttribute
     item_dimensions: ApiItemDimensions
-    release_date: models.SingleStringValuedAttribute
-    size: models.SingleStringValuedAttribute
-    unit_count: models.SingleIntegerValuedAttribute
+    release_date: ApiSingleStringValuedAttribute
+    size: ApiSingleStringValuedAttribute
+    unit_count: ApiSingleIntegerValuedAttribute
 
 class ApiTechnicalInfo(models.TechnicalInfo):
     formats: ApiMultiValuedAttributeStr
-    energy_efficiency_class: models.SingleStringValuedAttribute
+    energy_efficiency_class: ApiSingleStringValuedAttribute
+
+class ApiTradeInPrice(models.TradeInPrice):
+    amount: float
+    currency: str
+    display_amount: str
 
 class ApiTradeInInfo(models.TradeInInfo):
     is_eligible_for_trade_in: bool
-    price: models.TradeInPrice
+    price: ApiTradeInPrice
 
 class ApiItemInfo(models.ItemInfo):
     by_line_info: ApiByLineInfo
@@ -83,12 +110,33 @@ class ApiItemInfo(models.ItemInfo):
     manufacture_info: ApiManufactureInfo
     product_info: ApiProductInfo
     technical_info: ApiTechnicalInfo
-    title: models.SingleStringValuedAttribute
+    title: ApiSingleStringValuedAttribute
     trade_in_info: ApiTradeInInfo
+
+"""Offers model"""
+class ApiOfferCondition(models.OfferCondition):
+    sub_condition: models.OfferSubCondition
+    condition_note: models.OfferConditionNote
+
+class ApiListings(models.OfferListing):
+    availability: models.Availability
+    condition: ApiOfferCondition
+    delivery_info: models.OfferDeliveryInfo
+    id: str
+    is_buy_box_winner: bool
+
+
+class ApiOffers(models.Offers):
+    listings: list[ApiListings]
+
+
+"""Browse node info model"""
+class ApiBrowseNodeInfo(models.BrowseNodeInfo):
+    pass
 
 """Main model"""
 class ApiItem(models.Item):
     item_info: ApiItemInfo
     images: ApiImages
-    offers: models.Offers
-    browse_node_info: models.BrowseNodeInfo
+    offers: ApiOffers
+    browse_node_info: ApiBrowseNodeInfo
