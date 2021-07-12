fb3b7e8e7120a64d5e897c672bb7629d241dfeca
Sergio Abad <agutrot93@gmail.com>
2021-07-12 21:13:07 +0200

    Added BrowseNode model

---
+++ amazon_paapi/models/browse_nodes_result.py
@@ -0,0 +1,20 @@
+from ..sdk import models
+
+
+class BrowseNodeChild(models.BrowseNodeChild):
+    context_free_name: str
+    display_name: str
+    id: str
+
+
+class BrowseNodeAncestor(BrowseNodeChild, models.BrowseNodeAncestor):
+    ancestor: BrowseNodeChild
+
+
+class BrowseNode(models.BrowseNode):
+    display_name: str
+    id: str
+    is_root: bool
+    context_free_name: str
+    children: list[BrowseNodeChild]
+    ancestor: BrowseNodeAncestor
