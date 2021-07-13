a3f03b029a382a7af10c7d8a6a8fccf40fd38541
Sergio Abad <agutrot93@gmail.com>
2021-07-13 19:01:10 +0200

    Updated version to 4.0.0 and removed examples

--- example.py
+++
@@ -1,8 +0,0 @@
-from amazon_paapi.sdk.models import condition, merchant
-from amazon_paapi import AmazonApi
-import secrets
-from amazon_paapi.models import Condition
-
-amazon = AmazonApi(secrets.KEY, secrets.SECRET, secrets.TAG, secrets.COUNTRY)
-product = amazon.get_items('B01N5IB20Q', condition=Condition.NEW)
-print(product[0].item_info.title.display_value)
