0a06e02bb08ec8fee47ce7ac9eff6cad61756e9f
Sergio Abad <agutrot93@gmail.com>
2022-10-09 17:45:53 +0200

    Update version automatically based on tag name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setuptools.setup(
     name="python-amazon-paapi",
-    version="4.3.2",
+    version="<PACKAGE_VERSION>",
     author="Sergio Abad",
     author_email="sergio.abad@bytelix.com",
     description="Amazon Product Advertising API 5.0 wrapper for Python",
