71222d4171581977f690ace16dbfb4cf4e62e0eb
Sergio Abad <agutrot93@gmail.com>
2024-10-10 00:07:02 +0200

    created integration test for varios api methods

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -1,17 +1,42 @@
+from __future__ import annotations
+
 import os
 from unittest import TestCase, skipUnless
 
+from amazon_paapi.api import AmazonApi
 
-def are_credentials_defined() -> bool:
+
+def get_api_credentials() -> tuple[str]:
     api_key = os.environ.get("API_KEY")
     api_secret = os.environ.get("API_SECRET")
     affiliate_tag = os.environ.get("AFFILIATE_TAG")
     country_code = os.environ.get("COUNTRY_CODE")
 
-    return all([api_key, api_secret, affiliate_tag, country_code])
+    return api_key, api_secret, affiliate_tag, country_code
 
 
-@skipUnless(are_credentials_defined(), "Needs Amazon API credentials")
+@skipUnless(all(get_api_credentials()), "Needs Amazon API credentials")
 class IntegrationTest(TestCase):
-    def test_it_works(self):
-        self.assertTrue(True)
+    @classmethod
+    def setUpClass(cls):
+        api_key, api_secret, affiliate_tag, country_code = get_api_credentials()
+        cls.api = AmazonApi(api_key, api_secret, affiliate_tag, country_code)
+        cls.affiliate_tag = affiliate_tag
+
+    def test_search_items_and_get_info_and_variations_for_the_first_one(self):
+        search_result = self.api.search_items(keywords="zapatillas")
+        searched_item = search_result.items[0]
+
+        self.assertEqual(10, len(search_result.items))
+        self.assertIn(self.affiliate_tag, searched_item.detail_page_url)
+
+        get_results = self.api.get_items(searched_item.asin)
+
+        self.assertEqual(1, len(get_results))
+        self.assertIn(self.affiliate_tag, get_results[0].detail_page_url)
+
+        variations_result = self.api.get_variations(searched_item.asin)
+        variation_item = variations_result.items[0]
+
+        self.assertIn(self.affiliate_tag, variation_item.detail_page_url)
+        self.assertNotEqual(searched_item.asin, variation_item.asin)
