d9725a02f36c08041dcb79a006bb4eceba2d7eb6
Sergio Abad <agutrot93@gmail.com>
2026-01-11 17:14:41 +0100

    updated readthedocs config

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "myst_parser"]
 autodoc_typehints = "none"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+# templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
