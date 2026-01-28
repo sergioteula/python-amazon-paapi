074f8f88132d474e0f5ecf7e7704c6a954ec407e
Sergio Abad <agutrot93@gmail.com>
2026-01-29 00:20:17 +0100

    feat: Expose SDK model classes via a new `models` submodule.

--- amazon_creatorsapi/__init__.py
+++ amazon_creatorsapi/__init__.py
@@ -4,7 +4,8 @@ A Python wrapper for the Amazon Creators API.
 """
 
 __author__ = "Sergio Abad"
-__all__ = ["AmazonCreatorsApi", "Country"]
+__all__ = ["AmazonCreatorsApi", "Country", "models"]
 
+from . import models
 from .api import AmazonCreatorsApi
 from .core import Country
